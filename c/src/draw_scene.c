1041845b74b2a7cc03f4257ee389925e392d0934
Brian Walshe <walshe.brian@gmail.com>
2025-06-22 17:28:06 +0100

    added sphere

--- c/src/draw_scene.c
+++ c/src/draw_scene.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "color.h"
@@ -7,7 +8,19 @@
 
 #define DIV(A, B) ((double)A) / (B)
 
+
+bool hit_sphere(point3 *center, double radius, ray *r) {
+    vec3 oc = sub(center, &r->origin);
+    double a = dot(&r->direction, &r->direction);
+    double b = -2.0 * dot(&r->direction, &oc);
+    double c = dot(&oc, &oc) - radius*radius;
+    double discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(ray *r) {
+    if (hit_sphere(&(point3){0,0,-1}, 0.5, r))
+        return (color) {1, 0, 0};
 
     vec3 unit_direction = unit_vec(&r->direction);
     double a = 0.5*(unit_direction.y + 1.0);
